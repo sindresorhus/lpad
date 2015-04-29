a57e0178675013975c363b20cf3f470d98fd6eb9
Sindre Sorhus <sindresorhus@gmail.com>
2015-04-30 02:26:43 +0700

    enforce LF

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 'use strict';
-var eol = require('os').EOL;
-
 module.exports = function (str, pad) {
-	return pad ? pad + String(str).split(eol).join(eol + pad) : str;
+	return pad ? pad + String(str).split(/\r?\n/).join('\n' + pad) : str;
 };
--- test.js
+++ test.js
@@ -4,4 +4,5 @@ var lpad = require('./');
 
 it('should pad string', function () {
 	assert.equal(lpad('foo\nbar', '    '), '    foo\n    bar');
+	assert.equal(lpad('foo\r\nbar', '    '), '    foo\n    bar');
 });
