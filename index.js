6d9ec85b00bb4e35f0282c575a45b36102d6c908
Steve Mao <maochenyan@gmail.com>
2015-06-15 14:17:21 +1000

    do not pad an empty line
    Fixes the issue in https://github.com/sindresorhus/grunt-concurrent/pull/59

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'use strict';
 module.exports = function (str, pad) {
-	return pad ? pad + String(str).split(/\r?\n/).join('\n' + pad) : str;
+	return pad ? String(str).split(/\r?\n/).map(function (line) {
+		return line ? pad + line : line;
+	}).join('\n') : str;
 };
