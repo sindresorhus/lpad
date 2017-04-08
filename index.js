5b520c726585595ca6cfeedd2b6ea31db1ea38f0
Sam Verschueren <sam.verschueren@gmail.com>
2017-04-08 19:26:15 +0200

    Fix linting and testing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 var splitLines = require('split-lines');
+
 module.exports = function (str, pad) {
 	return pad ? splitLines(String(str)).map(function (line) {
 		return line ? pad + line : line;
